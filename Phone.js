7529a700cd678e54a36fb161530afc913cf6b824
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-09-07 18:50:41 -0700

    second try

--- Phone.js
+++ Phone.js
@@ -1,9 +1,53 @@
-class Phone{
+// Create a Phone class with attributes phoneName, storageLimit, isAndroid, screenSize, and hasHeadPhoneJack. Create a function that prints each attribute. Create an object of the class in your main function.
 
-}
+// Begin the Class
+class Store {
+    constructor(phonestore) {
+        this.name = phonestore;
+        this.showcase = [];
+    };
+// Add phone model
+    showInventory(newPhone) {
+        this.showcase.push(newPhone);
+        console.log(`We current have these phone models to showcase.
+        ${newPhone.phoneName}
+        ${newPhone.phoneStorage}
+        ${newPhone.phoneScreenSize}`);
+ }
+}    
 
-function main() {
+class Phone {
+    constructor(name, limit, size) {
+        this.phoneName = name;
+        this.osType = "";
+        this.phoneStorage = limit;
+        this.phoneScreenSize = size;
+  }
+       phoneJack;
 
-}
+};
 
-main();
+let myStore = new Store("Phone Store");
+
+let phone1 = new Phone("Pear", "Android", "3GB", "3inches");
+phone1.phoneJack = true;
+let phone2 = new Phone("Orange", "Android", "8GB", "4inches");
+phone2.phoneJack = true;
+let phone3 = new Phone("Banana", "Android", "8GB", "4inches");
+phone3.phoneJack = true;
+let phone4 = new Phone("Granny Smith", "iPhone", "5GB", "5inches");
+phone4.phoneJack = false;
+let phone5 = new Phone("Honey-crisp", "iPhone", "19GB", "10inches");
+phone5.phoneJack = false;
+let phone6 = new Phone("Cosimc-crisp", "iPhone", "30GB", "12inches");
+phone6.phoneJack = false;
+
+
+myStore.showInventory(phone1);
+myStore.showInventory(phone2);
+myStore.showInventory(phone3);
+myStore.showInventory(phone4);
+myStore.showInventory(phone5);
+myStore.showInventory(phone6);
+
+// let operatingSystem = 
